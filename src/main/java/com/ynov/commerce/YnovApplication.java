b066bd0cdb50d2b643dae71be0c606474e36dc07
Bromain <377roro@gmail.com>
2023-10-13 15:14:46 +0200

    first commit

---
+++ src/main/java/com/ynov/commerce/YnovApplication.java
@@ -0,0 +1,13 @@
+package com.ynov.commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YnovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YnovApplication.class, args);
+	}
+
+}
